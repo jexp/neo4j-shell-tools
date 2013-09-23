788d2e75193c380b9eec3fdc5711f06a78cd4eb9
Michael Hunger <github@jexp.de>
2013-09-23 10:58:42 +0200

    cleanup before zipping

--- zip.sh
+++ zip.sh
@@ -1,3 +1,4 @@
+rm neo4j-shell-tools-*.zip
 mvn clean install dependency:copy-dependencies
 zip -j neo4j-shell-tools-1.9.zip target/import-tools-1.0-SNAPSHOT.jar target/dependency/opencsv-2.3.jar target/dependency/neo4j-geoff-1.7-SNAPSHOT.jar target/dependency/mapdb-0.9.3.jar 
 s3cmd put -P neo4j-shell-tools*.zip s3://dist.neo4j.org/jexp/shell/
