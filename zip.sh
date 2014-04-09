af58064c25f5a0c26d2148b47bfa02a1c7493b77
Michael Hunger <github@jexp.de>
2014-04-09 10:02:14 +0200

    version 2.0 for this branch

--- zip.sh
+++ zip.sh
@@ -1,4 +1,4 @@
 rm neo4j-shell-tools-*.zip
 mvn clean install dependency:copy-dependencies
-zip -j neo4j-shell-tools-2.1.zip target/import-tools-2.1-SNAPSHOT.jar target/dependency/opencsv-2.3.jar target/dependency/geoff-0.5.0.jar target/dependency/mapdb-0.9.3.jar 
+zip -j neo4j-shell-tools-2.0.zip target/import-tools-2.0-SNAPSHOT.jar target/dependency/opencsv-2.3.jar target/dependency/geoff-0.5.0.jar target/dependency/mapdb-0.9.3.jar 
 s3cmd put -P neo4j-shell-tools*.zip s3://dist.neo4j.org/jexp/shell/
