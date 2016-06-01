6ca85abe459672c1a026560eb7e98d7352202233
Eric Fulton <efulton@blueorigin.com>
2016-06-01 16:59:19 -0700

    Added binary input option using Kryo

---
+++ src/main/java/org/neo4j/shell/tools/imp/format/kryo/KryoSerializationTypes.java
@@ -0,0 +1,12 @@
+package org.neo4j.shell.tools.imp.format.kryo;
+
+/**
+ * Created by efulton on 5/5/16.
+ */
+public enum KryoSerializationTypes {
+    NODE,
+    RELATIONSHIP,
+    CONSTRAINT,
+    INDEX,
+    DUMP_END
+}
