f3824c78849c1f466613882905f74fcf45d502f4
Eric Fulton <efulton@blueorigin.com>
2016-06-15 10:58:46 -0700

    Adding reflectasm jar to zip.sh

--- zip.sh
+++ zip.sh
@@ -1,5 +1,5 @@
 export VERSION=2.3.3
 rm neo4j-shell-tools_*.zip
 mvn clean install dependency:copy-dependencies
-zip -j neo4j-shell-tools_$VERSION.zip target/import-tools-${VERSION}*.jar target/dependency/opencsv-*.jar target/dependency/geoff-*.jar target/dependency/mapdb-*.jar target/dependency/target/reflectasm-*.jar target/dependency/kryo-*.jar target/dependency/minlog-*.jar target/dependency/objenesis-*.jar
+zip -j neo4j-shell-tools_$VERSION.zip target/import-tools-${VERSION}*.jar target/dependency/opencsv-*.jar target/dependency/geoff-*.jar target/dependency/mapdb-*.jar target/dependency/target/reflectasm-*.jar target/dependency/kryo-*.jar target/dependency/minlog-*.jar target/dependency/objenesis-*.jar target/dependency/reflectasm-*.jar
 s3cmd put -P neo4j-shell-tools*.zip s3://dist.neo4j.org/jexp/shell/
