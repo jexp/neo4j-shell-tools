857d163f5f8fca16ab6b5ed146dfdc9d1322d6fa
Michael Hunger <github@jexp.de>
2016-07-01 09:54:38 +0200

    Update to Neo4j 3.1.x

--- zip.sh
+++ zip.sh
@@ -1,4 +1,4 @@
-export VERSION=3.0.3
+export VERSION=3.1.0
 rm neo4j-shell-tools_*.zip
 mvn clean install dependency:copy-dependencies
 zip -j neo4j-shell-tools_$VERSION.zip target/import-tools-${VERSION}*.jar target/dependency/opencsv-*.jar target/dependency/geoff-*.jar target/dependency/mapdb-*.jar target/dependency/kryo-*.jar target/dependency/reflectasm-*.jar target/dependency/minlog-*.jar target/dependency/objenesis-*.jar
